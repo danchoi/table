0a172c6fb5c9adc93690dfee8fce1271e929d053
Daniel Choi <dhchoi@gmail.com>
2014-12-03 14:15:12 -0500

    hardwired table making for space-separated input

--- Main.hs
+++ Main.hs
@@ -1,15 +1,31 @@
 module Main where
 import Data.List 
+import Data.Monoid
 import Text.Printf
 
 main = do 
     s <- getContents 
     let (header, rest) = table . map words . lines $ s
-    putStrLn $ printRow header 
-    mapM_ (putStrLn . printRow) rest
+    putStrLn $ printRow 1 header 
+    putStrLn $ printDivider 1 (map snd header)
+    mapM_ (putStrLn . printRow 1) rest
 
-printRow :: [(String, Int)] -> String
-printRow xs = intercalate " | " $ map printCell xs 
+printRow :: Int -> [(String, Int)] -> String
+printRow gutter xs = 
+    mconcat [ margin gutter ' '
+      , (intercalate " | " $ map printCell xs )
+      , margin gutter ' '] 
+
+margin :: Int -> Char -> String
+margin w c = take w $ repeat c
+
+printDivider :: Int -> [Int] -> String
+printDivider gutter widths = 
+    mconcat [margin gutter '-'
+      , (intercalate "+" 
+        $ map (\w -> take (w + 1) $ repeat '-') widths)
+      , margin gutter '-']
+
 
 printCell :: (String, Int)  -> String
 printCell (x, width) = printf fmt x 
