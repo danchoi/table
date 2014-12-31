ffdad11c95204beb8b3489b41b99dafac170d950
Daniel Choi <dhchoi@gmail.com>
2014-12-31 12:17:53 -0500

    more code comments

--- Main.hs
+++ Main.hs
@@ -12,6 +12,7 @@ main = do
     putStrLn $ printDivider 1 (map snd header)
     mapM_ (putStrLn . printRow 1) rest
 
+-- | prints a row of cells with dividers
 printRow :: Int -> [(String, Int)] -> String
 printRow gutter xs = 
     mconcat [ margin gutter ' '
@@ -28,14 +29,19 @@ printDivider gutter widths =
         $ map (\w -> take w $ repeat '-') widths)
       , margin gutter '-']
 
-
 printCell :: (String, Int)  -> String
 printCell (x, width) = printf fmt x 
     where fmt = "%" ++ show width ++ "s"
 
 
 -- assume for now a header row and rest
+{- Given a 2 dimensional table, generates a tuple:
 
+    One of the header, the rest for the rest of the values.
+    Each row is represented as [(String, Int)]. The first
+    value is the text content; the second is the normalized of the column width
+    for that cell. 
+-}
 table :: [[String]] -> ([(String, Int)], [[(String, Int)]])
 table xs = 
   let xs' = cells xs
@@ -43,11 +49,11 @@ table xs =
       rest = tail xs'
   in (header, rest)
 
--- generate cells with built-in widths:
+-- | Generate cells with normalized column widths:
 cells :: [[String]] -> [[(String, Int)]]
 cells = transpose . map colw . transpose 
 
--- add width to each cell in a column
+-- | Add width to each cell in a column
 colw :: [String] -> [(String, Int)]
 colw xs = let width = maximum . map length $ xs
   in map (\x -> (x, width)) xs
